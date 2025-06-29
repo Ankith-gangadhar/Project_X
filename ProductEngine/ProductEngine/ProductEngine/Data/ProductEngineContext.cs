e9f2f9e33e945a21342d6610973abd8b5074f484
Ankith-gangadhar <92665313+Ankith-gangadhar@users.noreply.github.com>
2025-06-29 23:17:37 +0530

    Update ProductEngineContext.cs

--- ProductEngine/ProductEngine/ProductEngine/Data/ProductEngineContext.cs
+++ ProductEngine/ProductEngine/ProductEngine/Data/ProductEngineContext.cs
@@ -5,6 +5,7 @@ namespace ProductEngine.Data
 {
     public class ProductEngineContext : DbContext
     {
+    
         public ProductEngineContext(DbContextOptions<ProductEngineContext> options) : base(options) { }
         public DbSet<Product> Products { get; set; }
     }
